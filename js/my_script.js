ca48714deab2cb333c320652e55048f4d5c8138f
Dop-dd <dopdd@yahoo.com>
2020-05-16 15:49:09 +0000

    all

---
+++ js/my_script.js
@@ -0,0 +1,12 @@
+/** function will only run when page is loaded and html redered */
+
+/* $(document).ready(function() {
+    $("h2")a("tex-decorationt", "underline");
+    $("ul").css("border", "solid 1px #ccc");
+});
+*/
+
+$(document).ready(function() {
+    $("h2").addClass("underline");
+    $("ul").addClass("border");
+});
